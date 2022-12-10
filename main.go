0630f665ec0492fee0868a46635355f454a47d5e
wnrome21 <nateromero001@gmail.com>
2022-12-09 21:55:15 -0800

    code without an interface

--- main.go
+++ main.go
@@ -2,6 +2,32 @@ package main
 
 import "fmt"
 
+type englishBot struct {}
+
+type spanishBot struct {}
+
 func main() {
-	fmt.Println("Hello World")
+	eb := englishBot{}
+	sb := spanishBot{}
+
+	printGreeting(eb)
+	printGreeting(sb)
+}
+
+func (englishBot) getGreeting() string {
+	//Very custon logic for agenerating an english greeting
+	return "Hello There!"
+}
+
+func (spanishBot) getGreeting() string {
+	// Custom logic for generating an english greeting
+	return "Qué Tál?"
+}
+
+func printGreeting(eb englishBot) {
+	fmt.Println(eb.getGreeting())
+}
+
+func printGreeting( sb spanishBot) {
+	fmt.Println(sb.getGreeting())
 }
